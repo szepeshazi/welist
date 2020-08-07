33a7f479c8f7c8d3d66ba054d7a728b2c3ab775b
András Szepesházi <szepeshazi@gmail.com>
2020-08-07 16:33:15 +0200

    (feat) connect to firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
+import 'package:welist/observable_input_value/observable_input.dart';
 import 'package:welist/we_list/we_list.dart';
 import 'package:welist/we_list_item/we_list_item.dart';
 
@@ -24,14 +24,38 @@ class WeListApp extends StatelessWidget {
 class WeListHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Provider<WeList>(
-      builder: (_) => WeList(),
+      create: (_) => WeList()..load(),
       child: Scaffold(
           appBar: AppBar(
-            title: const Text('Todos'),
+            title: const Text('Bevásárlás'),
           ),
           body: Column(
-            children: <Widget>[WeListView()],
+            children: <Widget>[WeListNewItem(), WeListView()],
           )));
+}
+
+class WeListNewItem extends StatelessWidget {
+  final ObservableInput oInput = ObservableInput();
+  final TextEditingController controller = TextEditingController(text: "");
+
+  @override
+  Widget build(BuildContext context) {
+    final WeList weList = Provider.of(context);
+    return Observer(
+        builder: (context) => Container(
+            padding: EdgeInsets.only(top: 6.0, bottom: 6.0),
+            margin: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextField(
+                autofocus: true,
+                onChanged: (String value) {
+                  oInput.value = value;
+                },
+                onSubmitted: (_) {
+                  weList.add(WeListItem(oInput.input));
+                  oInput.value = "";
+                  controller.value = TextEditingValue(text: "empty");
+                })));
+  }
 }
 
 class WeListView extends StatelessWidget {
@@ -61,9 +85,7 @@ class WeListView extends StatelessWidget {
                                   ],
                                 ),
                               ),
-                              Text(item.stateName,
-                                  textAlign: TextAlign.start,
-                                  overflow: TextOverflow.ellipsis)
+                              Text(item.stateName, textAlign: TextAlign.start, overflow: TextOverflow.ellipsis)
                             ]));
                   }),
             ));
--- lib/we_list_item/we_list_item.dart
+++ lib/we_list_item/we_list_item.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobx/mobx.dart';
 
 part 'we_list_item.g.dart';
@@ -5,8 +6,9 @@ part 'we_list_item.g.dart';
 class WeListItem = _WeListItem with _$WeListItem;
 
 abstract class _WeListItem with Store {
-
   final String name;
+
+  final DocumentReference reference;
 
   @observable
   int createdAt;
@@ -14,7 +16,18 @@ abstract class _WeListItem with Store {
   @observable
   int completedAt;
 
-  _WeListItem(this.name) : createdAt = DateTime.now().millisecondsSinceEpoch;
+  _WeListItem(this.name)
+      : reference = null,
+        createdAt = DateTime.now().millisecondsSinceEpoch;
+
+  _WeListItem.fromMap(Map<String, dynamic> data, {this.reference})
+      : assert(data['name'] != null),
+        assert(data['createdAt'] != null),
+        name = data['name'],
+        createdAt = data['createdAt'],
+        completedAt = data['completedAt'];
+
+  _WeListItem.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @computed
   bool get completed => completedAt != null;
