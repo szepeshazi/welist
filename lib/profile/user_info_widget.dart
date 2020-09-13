a2022f4d6e7696be78a63973c2724df089e47778
András Szepesházi <szepeshazi@gmail.com>
2020-09-13 13:27:21 +0200

    (feat) profile menu actions

--- lib/profile/user_info_widget.dart
+++ lib/profile/user_info_widget.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:welist/auth/auth.dart';
+
+import '../main.dart';
 
 class UserInfoWidget extends StatelessWidget {
   @override
@@ -11,22 +14,36 @@ class UserInfoWidget extends StatelessWidget {
     return Row(children: [
       Text(auth.user.displayName ?? auth.user.email.split("@").first),
       Container(padding: EdgeInsets.only(left: 10), child: Icon(Icons.account_circle)),
-      PopupMenuButton<int>(
+      PopupMenuButton<ProfileMenuItem>(
           itemBuilder: (context) => [
                 PopupMenuItem(
-                  value: 1,
+                  value: ProfileMenuItem.settings,
                   child: Row(children: [
                     Container(padding: EdgeInsets.only(left: 10, right: 10), child: Text("Profile")),
                     Icon(Icons.settings)
                   ]),
                 ),
                 PopupMenuItem(
-                    value: 2,
+                    value: ProfileMenuItem.logout,
                     child: Row(children: [
                       Container(padding: EdgeInsets.only(left: 10, right: 10), child: Text("Logout")),
                       Icon(Icons.exit_to_app)
                     ]))
-              ])
+              ],
+      onSelected: (ProfileMenuItem selected) {
+            switch(selected) {
+              case ProfileMenuItem.settings:
+               Navigator.pushNamed(context, Routes.home);
+                break;
+              case ProfileMenuItem.logout:
+                FirebaseAuth.instance.signOut();
+                break;
+            }
+      })
     ]);
   }
+}
+
+enum ProfileMenuItem {
+  settings, logout
 }
